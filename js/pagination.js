e36fa8bf66888d47629576b24d162b11ece91e68
Alice Yang <yangalice212@gmail.com>
2021-07-12 18:35:27 +0800

    import productModal & delProductModal update label for input id add .prevent

--- js/pagination.js
+++ js/pagination.js
@@ -4,23 +4,20 @@ export default {
     <ul class="pagination">
       <li class="page-item" :class="{'disabled' : !page.has_pre}">
         <a class="page-link" href="#" aria-label="Previous" 
-        @click="$emit('get-data', page.current_page-1)">
+        @click.prevent="$emit('get-data', page.current_page-1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
       <li class="page-item" :class="{'active' : item === page.current_page }" v-for="item in page.total_pages" :key="item">
         <a class="page-link" href="#" 
-        @click="$emit('get-data', item)">{{item}}</a>
+        @click.prevent="$emit('get-data', item)">{{item}}</a>
       </li>
       <li class="page-item" :class="{'disabled' : !page.has_next}">
         <a class="page-link" href="#" aria-label="Next" 
-        @click="$emit('get-data', page.current_page+1)">
+        @click.prevent="$emit('get-data', page.current_page+1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
     </ul>
   </nav>`,
-  created() {
-    console.log(this.page);
-  },
 };
---
+++ js/productModal.js
@@ -0,0 +1,123 @@
+export default {
+  
+  props: ["tempProduct","isNew"],
+  methods: {
+    createImages() {
+      //加上一個空陣列，讓 imagesUrl 為陣列形式
+      this.tempProduct.imagesUrl = [];
+      this.tempProduct.imagesUrl.push("");
+    },
+  },
+  template: `<div id="productModal" ref="productModal" class="modal fade" tabindex="-1">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 id="productModalLabel" class="modal-title">
+            <span v-if="isNew">新增產品</span>
+            <span v-else>編輯產品</span>
+          </h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="form-group">
+                <label for="imageUrl">主要圖片</label>
+                <input id="imageUrl" v-model="tempProduct.imageUrl" type="text" class="form-control"
+                  placeholder="請輸入圖片連結">
+                <img class="img-fluid" :src="tempProduct.imageUrl">
+              </div>
+              <div class="mb-1">多圖新增</div>
+              <div v-if="Array.isArray(tempProduct.imagesUrl)">
+                <div class="mb-1" v-for="(image, key) in tempProduct.imagesUrl" :key="key">
+                  <div class="form-group">
+                    <label :for="image[key]">圖片網址</label>
+                    <input :id="image[key]" v-model="tempProduct.imagesUrl[key]" type="text" class="form-control"
+                      placeholder="請輸入圖片連結">
+                  </div>
+                  <img class="img-fluid" :src="image">
+                </div>
+                <div v-if="!tempProduct.imagesUrl.length || tempProduct.imagesUrl[tempProduct.imagesUrl.length - 1]">
+                  <button class="btn btn-outline-primary btn-sm d-block w-100"
+                    @click="tempProduct.imagesUrl.push('')">
+                    新增圖片
+                  </button>
+                </div>
+                <div v-else>
+                  <button class="btn btn-outline-danger btn-sm d-block w-100" @click="tempProduct.imagesUrl.pop()">
+                    刪除圖片
+                  </button>
+                </div>
+              </div>
+              <div v-else>
+                <button class="btn btn-outline-primary btn-sm d-block w-100" @click="createImages">
+                  多圖新增
+                </button>
+              </div>
+            </div>
+            <div class="col-sm-8">
+              <div class="form-group">
+                <label for="title">標題</label>
+                <input id="title" v-model="tempProduct.title" type="text" class="form-control" placeholder="請輸入標題">
+              </div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="category">分類</label>
+                  <input id="category" v-model="tempProduct.category" type="text" class="form-control"
+                    placeholder="請輸入分類">
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="unit">單位</label>
+                  <input id="unit" v-model="tempProduct.unit" type="text" class="form-control" placeholder="請輸入單位">
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="origin_price">原價</label>
+                  <input id="origin_price" v-model.number="tempProduct.origin_price" type="number" min="0"
+                    class="form-control" placeholder="請輸入原價">
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="price">售價</label>
+                  <input id="price" v-model.number="tempProduct.price" type="number" min="0" class="form-control"
+                    placeholder="請輸入售價">
+                </div>
+              </div>
+              <hr>
+
+              <div class="form-group">
+                <label for="description">產品描述</label>
+                <textarea id="description" v-model="tempProduct.description" type="text" class="form-control"
+                  placeholder="請輸入產品描述">
+          </textarea>
+              </div>
+              <div class="form-group">
+                <label for="content">說明內容</label>
+                <textarea id="content" v-model="tempProduct.content" type="text" class="form-control"
+                  placeholder="請輸入說明內容">
+          </textarea>
+              </div>
+              <div class="form-group">
+                <div class="form-check">
+                  <input id="is_enabled" v-model="tempProduct.is_enabled" class="form-check-input" type="checkbox"
+                    :true-value="1" :false-value="0">
+                  <label class="form-check-label" for="is_enabled">是否啟用</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+            取消
+          </button>
+          <button type="button" class="btn btn-primary" @click="$emit('update-product', tempProduct)">
+            確認
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>`,
+}
